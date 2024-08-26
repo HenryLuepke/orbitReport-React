f59ae35245e1d786d38bf70a81130c7b30ab99ec
HenryLuepke <henryluepke4@gmail.com>
2024-08-26 16:34:01 -0500

    Tests passing, no styling

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,16 +1,30 @@
+function TableDisplay({ data }) {
+  const satInfo = (props) => {
+    return props.map((data, id) => 
+      <tr key = {id}>
+        <td>{data.name}</td>
+        <td>{data.type}</td>
+        <td>{data.launchDate}</td>
+        <td>{data.operational ? 'Satelite Active' : 'Satelite Inactive'}</td>
+      </tr>
+  );
+  }
+  return <tbody>{satInfo(data)}</tbody>
+}
+
 const Table = ({ sat }) => {
+
   return (
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satelite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
-     <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
+      <TableDisplay data={sat}/>
    </table>
   );
 };
